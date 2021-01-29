d558e85c0239958a913ea75de53d3be917a5f63c
raunak_k02 <raunakk728@gmail.com>
2021-01-30 02:47:05 +0530

    added firebase sdk by Raunak

--- src/app/mainPage/Navbar/firebase.js
+++ src/app/mainPage/Navbar/firebase.js
@@ -1,14 +1,14 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyA1IADtjMkm6S2ReGE_WqN910MEe8OvEK8",
-    authDomain: "imagre-repo.firebaseapp.com",
-    projectId: "imagre-repo",
-    storageBucket: "imagre-repo.appspot.com",
-    messagingSenderId: "324168114522",
-    appId: "1:324168114522:web:2953897a6796e59778d363",
-    measurementId: "G-G9QPX9C052"
-  };
+  apiKey: "AIzaSyDmGqp37MUK1hDcQlHTVvP4uz2wU46AJtE",
+  authDomain: "cosmic-gizmo-298307.firebaseapp.com",
+  projectId: "cosmic-gizmo-298307",
+  storageBucket: "cosmic-gizmo-298307.appspot.com",
+  messagingSenderId: "33194818513",
+  appId: "1:33194818513:web:8fc414e65450e338087ef0",
+  measurementId: "G-FL5Y0TTX4H"
+};
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
